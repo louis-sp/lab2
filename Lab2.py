513fd36d33da1e02bffb614b0d58828ebfd8f508
Louis Tang <louistang.25@ichat.sp.edu.sg>
2026-04-29 10:29:56 +0800

    hello

--- Lab2.py
+++ Lab2.py
@@ -1,2 +1,31 @@
 print ("ET0735 (DevOps for AIot) - Lab 2 -  Introduction to Python" \
 "")
+
+def cbmi ( height, weight):
+    print ("Height = "+str(height))
+    print ("Weight = "+str(weight))
+    bmi = weight / (height * height)
+    print (str(bmi))
+    if(bmi < 18.0):
+        print ("underweight")
+    elif (bmi > 25.0):
+        print ("overweight")
+    else:
+        print ("normal range")
+        
+
+
+    return bmi
+
+
+
+
+
+
+
+cbmi (height = 1.6,weight = 57)
+
+
+
+
+
