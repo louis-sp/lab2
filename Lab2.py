aae2f1d49e13e2c5be33ed475d36cc224bf7da8c
Louis Tang <louistang.25@ichat.sp.edu.sg>
2026-05-06 09:36:29 +0800

    ddd

--- Lab2.py
+++ Lab2.py
@@ -1,5 +1,50 @@
-def main():
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("enter some numbers seperated by commas")
+
+def get_user_input():
+    uinumbers = input()
+    uisplitnumbers = uinumbers.split(",")
+    uilist = [float(num) for num in uisplitnumbers]
+    print (uilist)
+    return (uilist)
+
+def calc_average(uilist):
+    total_count = len(uilist)
+    total = sum(uilist)
+    average = total / total_count
+    print (average)
+    return (total_count)
+    
+
+def find_min_max(uilist):
+    minui = min(uilist)
+    maxui = max(uilist)
+    print (minui)
+    print (maxui)   
+    return (minui,maxui)
+
+def sort_temperature(uilist):
+    sort_temp = uilist.sort
+    print (sort_temp)
+    return (sort_temp)
+
+def calc_median_temperature(uilist,total_count):
+    sortuilist = sorted(uilist)
+    medianpos = total_count % 2
+    if medianpos == 1 :
+        odd = total_count // 2 
+        oddpos = int(medianpos)
+        print (uilist[oddpos])
+        return (uilist[oddpos])
+    else: 
+        odd = total_count // 2
+        oddpos = int(medianpos)
+        evenpos = oddpos - 1
+        middle = (uilist[oddpos] + uilist[evenpos]) / 2
+        print (middle)
+        return(middle)
+
 
 
 
@@ -15,16 +60,20 @@ def cbmi ( height, weight):
     else:
         print ("normal range")
     return bmi
+
+
+    cbmi (height = 1.6,weight = 57)
+
 def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+display_main_menu()
+uilist = get_user_input()
+total_count = calc_average (uilist)
+find_min_max(uilist)
+sort_temperature(uilist)
+calc_median_temperature(uilist,total_count)
 
-
-
-
-
-
-cbmi (height = 1.6,weight = 57)
-
-
-
+if __name__ == "__main__":
+    main()
 
 
